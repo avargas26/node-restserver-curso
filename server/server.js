3eddbebbe90ebaa9d0d7ebce47174d1008ca265c
Andrés Vargas <andvargas25@gmail.com>
2020-08-26 16:28:35 -0400

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require('./config/config');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.send('Hello World')
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'El nombre es necesario'
+        });
+    } else {
+        res.send({
+            persona: body
+        });
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto ' + process.env.PORT);
+})
