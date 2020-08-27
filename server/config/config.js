028375551d8c369420617a88fe813c5136a45b7f
Andrés Vargas <andvargas25@gmail.com>
2020-08-27 18:24:51 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//entornos
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:HXd6GnkeRwskGetv@cluster0.dmisp.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const usuario = require('../models/usuario');
+
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+            });
+
+        });
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    //ESTO ES UN CALLBACK
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        //elimina el valor de retorno
+        //usuarioDB.password = null;
+        usuarioDB.password = undefined
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
+});
+app.put('/usuario/:id', function(req, res) {
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    let id = req.params.id;
+    /*esta es na forma de eliminar los campos k no se deben actualizar
+    delete body.password;
+    delete body.google;
+    */
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+app.delete('/usuario/:id', function(req, res) {
+    let cambiarEstado = {
+        estado: false
+    };
+    let id = req.params.id;
+    //eliminar registro de la bd
+    /*Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            })
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });*/
+    Usuario.findByIdAndUpdate(id, cambiarEstado, { new: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+module.exports = app;
